57487677b5209a0e43b3530dc43cceb59e5b88ec
Guillaume Delanoy <gdelanoy@gmail.com>
2020-05-15 13:56:55 +0200

    firewall needs cooling

---
+++ config/cairo-dock/third-party/Liferea/util.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+# This is a part of the external Liferea applet for Cairo-Dock
+#
+# Author: Eduardo Mucelli Rezende Oliveira
+# E-mail: edumucelli@gmail.com or eduardom@dcc.ufmg.br
+
+from __future__ import print_function
+
+def log (string):
+    print("[+] Liferea: %s" % string)
